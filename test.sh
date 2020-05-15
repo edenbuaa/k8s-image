ff0d76051eee48168169043df41be913400b42df
edenbuaa <edenbuaa@outlook.com>
2020-05-15 15:53:55 +0800

    Update test.sh
    u

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #docker pull gcr.io/knative-releases/knative.dev/serving/cmd/queue@sha256:3932262d4a44284f142f4c49f707526e70dd86317163a88a8cbb6de035a401a9
 docker images -q | grep gcr.io/knative-releases/knative.dev/serving/cmd/queue@sha256:3932262d4a44284f142f4c49f707526e70dd86317163a88a8cbb6de035a401a9
+echo '2'
+docker images | grep gcr.io/knative-releases/knative.dev/serving/cmd/queue@sha256:3932262d4a44284f142f4c49f707526e70dd86317163a88a8cbb6de035a401a9 | awk {'print $3'}
 
